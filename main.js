ed1362fe5e302d7278aac0e9aa562e30b54b605c
ramseypapii <ramsey.kuwona@student.moringaschool.com>
2023-10-18 11:29:30 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,6 +3,43 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  const emptyHearts = document.querySelectorAll(".like-glyph");
+  
+  emptyHearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          // Change the heart to full
+          heart.classList.add("activated-heart");
+        })
+        .catch(() => {
+          // Display the error modal
+          errorModal.classList.remove("hidden");
+          // Display the error message
+          const errorMessage = document.getElementById("modal-message");
+          errorMessage.innerText = "Server Error!";
+          // Hide the error modal after 3 seconds
+          const errorModal = document.getElementById("modal");
+
+          setTimeout(() => {
+            
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+
+  const fullHearts = document.querySelectorAll(".activated-heart");
+  fullHearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      // Change the heart back to empty
+      heart.classList.remove("activated-heart");
+    });
+  });
+});
+
 
 
 
